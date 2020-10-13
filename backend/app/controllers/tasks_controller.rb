9f2838a901df06d418c045764ae465421c754f53
fenglisa <lisafeng10@gmail.com>
2020-10-13 11:56:18 -0400

    sort tasks by due date

--- backend/app/controllers/tasks_controller.rb
+++ backend/app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
 
   # GET /tasks
   def index
-    @tasks = Task.all
+    @tasks = Task.all.by_due_date
     options = {
       include: [:user]
     }
--- backend/app/models/task.rb
+++ backend/app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
   validates :title, presence: true
   belongs_to :user
+  scope :by_due_date, -> {order(due: :asc)}
 end
