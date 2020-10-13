c035eb2c1919ac0a603d5f6f65f7cc444201f60a
fenglisa <lisafeng10@gmail.com>
2020-10-13 11:56:59 -0400

    new user form

--- frontend/index.js
+++ frontend/index.js
@@ -13,7 +13,7 @@
 // }
 
 fetch("http://localhost:3000/tasks").then(r => r.json()).then(info => {
-  // console.log(info);
+  console.log(info);
   info.data.forEach((t) => {
     const u = info.included.find(u => u.id === t.relationships.user.data.id);
     // renderTask(t, u);
@@ -26,7 +26,7 @@ fetch("http://localhost:3000/users").then(r => r.json()).then(data => {
   data.forEach((u) => {
     const opt = document.createElement('option')
     opt.setAttribute("value", `${u.id}`);
-    var t = document.createTextNode(`${u.name}`);
+    const t = document.createTextNode(`${u.name}`);
     opt.appendChild(t);
     users.appendChild(opt)
   })
@@ -46,7 +46,7 @@ fetch("http://localhost:3000/users").then(r => r.json()).then(data => {
 //     })
 // }
 
-const handleForm = (e) => {
+const handleTaskForm = (e) => {
   e.preventDefault();
   const task = new TaskToDo({relationships: {user: {data: {user_id: users.value}}}, attributes: {title: title.value, due: due.value}});
   task.persist();
@@ -56,7 +56,15 @@ const handleForm = (e) => {
   // e.target.reset();
 }
 
-newTaskForm.addEventListener('submit', handleForm )
+newTaskForm.addEventListener('submit', handleTaskForm )
+
+const handleUserForm = (e) => {
+  e.preventDefault();
+  const user = new TaskMaster({name: username.value});
+  user.persist();
+}
+
+newUserForm.addEventListener('submit', handleUserForm )
 
 class TaskToDo {
 
@@ -68,28 +76,45 @@ class TaskToDo {
   }
 
   display(u){
-    const li = document.createElement('li');
-    li.classList.add("task");
+    const tr = document.createElement('tr')
+    tr.classList.add("task")
     if(this.due){
       const today = new Date();
       if(new Date(this.due) < today){
-        li.classList.add("task-overdue");
+        tr.classList.add("task-overdue");
       }
     }
-    li.innerText =  this.title + ` -- ${u.attributes.name}`;
-    document.body.appendChild(li);
-    li.addEventListener('click', function(){
-      this.classList.toggle("task-complete");
-    });
     const button = document.createElement('button');
     button.innerText = "X";
     button.addEventListener('click', (e) => this.delete(e));
+    tr.innerHTML =  `<td>${this.title}</td><td>${u.attributes.name}</td><td>${this.due}</td>`;
+    tr.appendChild(button);
+    toDoTable.appendChild(tr);
+    tr.addEventListener('click', function(){
+      this.classList.toggle("task-complete");
+    });
+    // const li = document.createElement('li');
+    // li.classList.add("task");
+    // if(this.due){
+    //   const today = new Date();
+    //   if(new Date(this.due) < today){
+    //     li.classList.add("task-overdue");
+    //   }
+    // }
+    // li.innerText =  this.title + ` -- ${u.attributes.name}`;
+    // document.body.appendChild(li);
+    // li.addEventListener('click', function(){
+    //   this.classList.toggle("task-complete");
+    // });
+    // const button = document.createElement('button');
+    // button.innerText = "X";
+    // button.addEventListener('click', (e) => this.delete(e));
     // (e) => {
     //   fetch(`http://localhost:3000/tasks/${this.id}`, {
     //     method: "DELETE"
     //   }).then(li.remove())
     // });
-    li.appendChild(button);
+    // li.appendChild(button);
   }
 
   persist(){
@@ -117,5 +142,36 @@ class TaskToDo {
       method: "DELETE"
     }).then(e.target.parentElement.remove())
 
+  }
+}
+
+class TaskMaster {
+
+  constructor({id, name}){
+    this.name = name;
+    this.id = id;
+  }
+
+  persist(){
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({user: {name: this.name}})
+    }).then(r => r.json()).then(info => {
+        if(info.errors){
+          console.log(info.errors);
+        }else{
+          newUserForm.reset();
+          this.id = info.id;
+          const opt = document.createElement('option')
+          opt.setAttribute("value", `${this.id}`);
+          var t = document.createTextNode(`${this.name}`);
+          opt.appendChild(t);
+          users.appendChild(opt)
+        }
+      })
   }
 }
