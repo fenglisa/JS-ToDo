af7e7cdb8f98f238eeaf7c1f36dd833676b5e7b3
fenglisa <lisafeng10@gmail.com>
2020-10-14 14:02:16 -0400

    unique user validator

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
+  before_validation :normalize_name
   has_many :tasks
+
+  private
+  def normalize_name
+    self.name = name.downcase.titleize
+  end
 end
