cf5cdcb12cc930a5a2698f9d401874375f94a745
fenglisa <lisafeng10@gmail.com>
2020-10-15 09:25:52 -0400

    modified user controller

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -9,9 +9,9 @@ class UsersController < ApplicationController
   end
 
   # GET /users/1
-  def show
-    render json: @user
-  end
+  # def show
+  #   render json: @user
+  # end
 
   # POST /users
   def create
@@ -25,25 +25,20 @@ class UsersController < ApplicationController
   end
 
   # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @user.update(user_params)
+  #     render json: @user
+  #   else
+  #     render json: @user.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
+  # def destroy
+  #   @user.destroy
+  # end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
     # Only allow a trusted parameter "white list" through.
     def user_params
       params.require(:user).permit(:name)
