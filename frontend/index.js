1004c761c8610636866b44165e2eaee9aa5c3be1
fenglisa <lisafeng10@gmail.com>
2021-03-06 22:03:09 -0500

    cleanup index.js

--- frontend/index.js
+++ frontend/index.js
@@ -12,8 +12,13 @@
 //     li.appendChild(button);
 // }
 
+// Dark Mode Button
+const button = document.createElement('button');
+button.innerText = 'Dark Mode';
+button.addEventListener('click', () => document.body.style.backgroundColor = "grey");
+document.body.appendChild(button);
+
 fetch("http://localhost:3000/tasks").then(r => r.json()).then(info => {
-  // console.log(info);
   info.data.forEach((t) => {
     const u = info.included.find(u => u.id === t.relationships.user.data.id);
     // renderTask(t, u);
@@ -29,28 +34,10 @@ fetch("http://localhost:3000/users").then(r => r.json()).then(data => {
   })
 })
 
-// const createTask = (taskInfo) => {
-//   return fetch("http://localhost:3000/tasks", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       "Accept": "application/json"
-//     },
-//     body: JSON.stringify(taskInfo)
-//   }).then(r => r.json()).then(info => {
-//       const u = info.included[0];
-//       renderTask(info.data, u);
-//     })
-// }
-
 const handleTaskForm = (e) => {
   e.preventDefault();
   const task = new TaskToDo({relationships: {user: {data: {user_id: users.value}}}, attributes: {title: title.value, due: due.value}});
   task.persist();
-
-  // const {title, due, name} = e.target;
-  // createTask({task: {title: title.value, due: due.value, user_id: name.value}})
-  // e.target.reset();
 }
 
 newTaskForm.addEventListener('submit', handleTaskForm )
@@ -102,28 +89,6 @@ class TaskToDo {
     tr.addEventListener('click', function(){
       this.classList.toggle("task-complete");
     });
-    // const li = document.createElement('li');
-    // li.classList.add("task");
-    // if(this.due){
-    //   const today = new Date();
-    //   if(new Date(this.due) < today){
-    //     li.classList.add("task-overdue");
-    //   }
-    // }
-    // li.innerText =  this.title + ` -- ${u.attributes.name}`;
-    // document.body.appendChild(li);
-    // li.addEventListener('click', function(){
-    //   this.classList.toggle("task-complete");
-    // });
-    // const button = document.createElement('button');
-    // button.innerText = "X";
-    // button.addEventListener('click', (e) => this.delete(e));
-    // (e) => {
-    //   fetch(`http://localhost:3000/tasks/${this.id}`, {
-    //     method: "DELETE"
-    //   }).then(li.remove())
-    // });
-    // li.appendChild(button);
   }
 
   persist(){
